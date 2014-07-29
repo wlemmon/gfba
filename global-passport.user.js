9629d5393b23fb5cf1dc154c546d62dba41747b4
unknown <lemmonw@SEA-1201016670.ant.amazon.com>
2014-07-29 14:45:41 -0700

    new file.

--- global-passport.user.js
+++ global-passport.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name       Global Passport
-// @namespace  https://rawgit.com/wlemmon/gfba/master/global-passport.user.js
+// @namespace    https://rawgit.com/wlemmon/gfba/master/global-passport.user.js
+// @downloadURL  https://rawgit.com/wlemmon/gfba/master/global-passport.user.js
+// @updateURL    https://rawgit.com/wlemmon/gfba/master/global-passport.user.js
 // @version    0.1
 // @description  enter something useful
 // @include     /^https?://.*\.com(:\d+)?/gp/.*$/
@@ -12,5 +14,5 @@ $(document).ready(
     	$("#sc-top-nav-root").append(
             '<li class="sc-level1 sc-drop-nav sc-hover-nav "> \
                  <!--[if IE 6]><iframe src="javascript:false;" class="sc-nav-iframeShim"></iframe><![endif]--> \
-                 <a href="https://'+document.domain+'/gp/global-selling/features/global-passport.html" class="sc-menu-trigger sc-tab-a">Global Passport</a></li>');
+                 <a href="'+location.origin+'/gp/global-selling/features/global-passport.html" class="sc-menu-trigger sc-tab-a">Global Passport</a></li>');
     });
